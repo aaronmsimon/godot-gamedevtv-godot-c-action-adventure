bc6765fae67abe555611d02c15fb4f8a72ec3dbf
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2024-03-12 21:34:11 -0700

    Moving the Player (6_PM_GRA)

--- Dungeon RPG/Scripts/Characters/Player/Player.cs
+++ Dungeon RPG/Scripts/Characters/Player/Player.cs
@@ -3,15 +3,20 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    private Vector2 direction = new();
+
     public override void _PhysicsProcess(double delta)
     {
-        // base._PhysicsProcess(delta);
-        GD.Print("Player physics process!");
+        Velocity = new(direction.X, 0, direction.Y);
+        Velocity *= 5;
+
+        MoveAndSlide();
     }
 
     public override void _Input(InputEvent @event)
     {
-        // base._Input(@event);
-        GD.Print("Player input!");
+        direction = Input.GetVector(
+            "MoveLeft", "MoveRight", "MoveForward", "MoveBackward"
+        );
     }
 }
