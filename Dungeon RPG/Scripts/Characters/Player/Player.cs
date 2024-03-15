92de2cf92d225c7e768b62578474d0485fc00565
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2024-03-14 21:55:49 -0700

    Creating an Idle and Move State (3_SM_GRA)

--- Dungeon RPG/Scripts/Characters/Player/Player.cs
+++ Dungeon RPG/Scripts/Characters/Player/Player.cs
@@ -4,15 +4,10 @@ using System;
 public partial class Player : CharacterBody3D
 {
     [ExportGroup("Required Nodes")]
-    [Export] private AnimationPlayer animPlayerNode;
-    [Export] private Sprite3D sprite3DNode;
+    [Export] public AnimationPlayer animPlayerNode;
+    [Export] public Sprite3D sprite3DNode;
 
     private Vector2 direction = new();
-
-    public override void _Ready()
-    {
-        animPlayerNode.Play(GameConstants.ANIM_IDLE);
-    }
 
     public override void _PhysicsProcess(double delta)
     {
@@ -31,15 +26,6 @@ public partial class Player : CharacterBody3D
             GameConstants.INPUT_MOVEFORWARD,
             GameConstants.INPUT_MOVEBACKWARD
         );
-
-        if (direction == Vector2.Zero)
-        {
-            animPlayerNode.Play(GameConstants.ANIM_IDLE);
-        }
-        else
-        {
-            animPlayerNode.Play(GameConstants.ANIM_MOVE);
-        }
     }
 
     private void Flip()
